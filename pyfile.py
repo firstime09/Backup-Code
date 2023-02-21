2fc2cb3c2c7521795dab0faf4995e0767caad449
FFTampinongkol <fftampinongkol09@gmail.com>
2023-02-21 09:08:53 +0700

    N

--- pyfile.py
+++ pyfile.py
@@ -1,2 +1,11 @@
 import streamlit as st
-st.write("""Welcome to Streamlit""")
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_excel(io='supermarkt_sales.xlsx', engine='openpyxl', sheet_name='Sales',
+                        skiprows=3, usecols='B:R', nrows=1000)
+
+#st.write("""Welcome to Streamlit""")
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+
+st.dataframe(df)
