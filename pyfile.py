0e24011dff5624e64ce3a9334eb0de02532ed8a1
FFTampinongkol <fftampinongkol09@gmail.com>
2023-02-21 10:05:23 +0700

    a

--- pyfile.py
+++ pyfile.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-# import plotly.express as px
+import plotly.express as px
 
 df = pd.read_excel(io='supermarkt_sales.xlsx', engine='openpyxl', sheet_name='Sales',
                         skiprows=3, usecols='B:R', nrows=1000)
