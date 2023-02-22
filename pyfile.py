f4bd3bea9c2fc9e70b8d199a9fbcf589099f726b
FFTampinongkol <fftampinongkol09@gmail.com>
2023-02-22 11:03:33 +0700

    Update

--- pyfile.py
+++ pyfile.py
@@ -27,3 +27,25 @@ def dtFrame(dataSet):
 x, y = dtFrame(dataSet)
 st.write("Shape of data", x.shape)
 st.write("Number of class", len(np.unique(y)))
+
+def tambah_parm(parm):
+    parms = dict()
+    if parm == 'K Nears Neighbor':
+        K = st.sidebar.slider('K', 1, 15)
+        parms['K'] = K
+    elif parm == 'Support Vector Machine':
+        C = st.sidebar.slider('C', 0.01, 10.0)
+        parms['C'] = C
+    elif parm == 'Neural Network':
+        epoch = st.sidebar.slider('epoch', 100, 300)
+        parms['epoch'] = epoch
+    else:
+        max_depth = st.sidebar.slider('max_depth', 2, 15)
+        parms['max_depth'] = max_depth
+        n_estimator = st.sidebar.slider('n_estimators', 1, 100)
+        parms['n_estimators'] = n_estimator
+    return parms
+
+params = tambah_parm(className)
+
+# def pilih_class(algorithm, params):
