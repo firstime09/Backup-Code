3dcbee006cb426b69f5d708c41a2426d334cf265
FFTampinongkol <fftampinongkol09@gmail.com>
2023-02-22 18:02:35 +0700

    Update

---
+++ mylib/module.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+import numpy as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import datasets
+
+class myfunction():
+
+    def dtFrame(dataSet):
+        if dataSet == "Iris":
+            data = datasets.load_iris()
+        elif dataSet == "Breast Cancer":
+            data = datasets.load_breast_cancer()
+        else:
+            data = datasets.load_wine()
+        x = data.data
+        y = data.target
+        return x, y
+    
+    def tambah_parm(parm):
+        parms = dict()
+        if parm == 'K Nears Neighbor':
+            K = st.sidebar.slider('K', 1, 15)
+            parms['K'] = K
+        elif parm == 'Support Vector Machine':
+            C = st.sidebar.slider('C', 0.01, 10.0)
+            parms['C'] = C
+        elif parm == 'Neural Network':
+            alpha = st.sidebar.slider('alpha', 0.001, 0.9)
+            parms['alpha'] = alpha
+        else:
+            max_depth = st.sidebar.slider('max_depth', 2, 15)
+            parms['max_depth'] = max_depth
+            n_estimator = st.sidebar.slider('n_estimators', 1, 100)
+            parms['n_estimators'] = n_estimator
+        return parms
+    
+    def pilih_class(algorithm, params):
+        algo = None
+        if algorithm == 'K Nears Neighbor':
+            algo = KNeighborsClassifier(n_neighbors=params['K'])
+        elif algorithm == 'Support Vector Machine':
+            algo = SVC(kernel='rbf', C=params['C'])
+        elif algorithm == 'Neural Network':
+            algo = MLPClassifier(alpha=params['alpha'], random_state=1234)
+        else:
+            algo = RandomForestClassifier(n_estimators=params['n_estimators'],
+                                      max_depth=params['max_depth'], random_state=1234)
+        return algo
--- pyfile.py
+++ pyfile.py
@@ -1,51 +1,40 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
-from sklearn import datasets
+import pandas as pd
+from io import StringIO
+from mylib.module import myfunction as mf
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
 
 st.title("Streamlit Test")
 st.write("""Machine Learning Classifier""")
+
+# uploaded_files = st.sidebar.file_uploader("Choose a CSV file", accept_multiple_files=True,)
+uploaded_file = st.sidebar.file_uploader("Choose a xlsx file")
+if uploaded_file is not None:
+    df0 = pd.ExcelFile(uploaded_file)
+    df1 = pd.read_excel(df0)
+    st.dataframe(df1)
 
 dataSet = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine Dataset",
                                           "Supermarket Sales"))
 
 className = st.sidebar.selectbox("Select Classifier", ("K Nears Neighbor", "Support Vector Machine", "Random Forest",
                                                        "Neural Network"))
-# st.write(dataSet)
 
-def dtFrame(dataSet):
-    if dataSet == "Iris":
-        data = datasets.load_iris()
-    elif dataSet == "Breast Cancer":
-        data = datasets.load_breast_cancer()
-    else:
-        data = datasets.load_wine()
-    x = data.data
-    y = data.target
-    return x, y
-
-x, y = dtFrame(dataSet)
+x, y = mf.dtFrame(dataSet)
 st.write("Shape of data", x.shape)
 st.write("Number of class", len(np.unique(y)))
 
-def tambah_parm(parm):
-    parms = dict()
-    if parm == 'K Nears Neighbor':
-        K = st.sidebar.slider('K', 1, 15)
-        parms['K'] = K
-    elif parm == 'Support Vector Machine':
-        C = st.sidebar.slider('C', 0.01, 10.0)
-        parms['C'] = C
-    elif parm == 'Neural Network':
-        epoch = st.sidebar.slider('epoch', 100, 300)
-        parms['epoch'] = epoch
-    else:
-        max_depth = st.sidebar.slider('max_depth', 2, 15)
-        parms['max_depth'] = max_depth
-        n_estimator = st.sidebar.slider('n_estimators', 1, 100)
-        parms['n_estimators'] = n_estimator
-    return parms
+params = mf.tambah_parm(className)
+algo = mf.pilih_class(className, params)
 
-params = tambah_parm(className)
+X_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1234)
 
-# def pilih_class(algorithm, params):
+algo = algo.fit(X_train, y_train)
+y_pred = algo.predict(x_test)
+
+acc = accuracy_score(y_test, y_pred)
+st.write(f'Algoritma = {algo}')
+st.write(f'Akurasi = ', acc)
