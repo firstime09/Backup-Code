d153f3e131e85f0fd1eae1dd58cee3d588c1478d
FFTampinongkol <fftampinongkol09@gmail.com>
2023-02-24 18:50:11 +0700

    up

--- stapp_.py
+++ stapp_.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Hello World!")
+
+st.success("Success")
+st.info("Information")
+st.warning("Warning")
+st.error("Error")
+ 
+exp = ZeroDivisionError("Trying to divide by Zero")
+st.exception(exp)
